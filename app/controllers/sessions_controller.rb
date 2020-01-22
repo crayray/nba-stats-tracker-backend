bc6aa1daad35c5ece084f71696349c0bf8cf53d5
rachfuerst <rach.fuerst@gmail.com>
2020-01-22 17:29:43 -0600

    Further auth work. So far the broken piece is the /sessions route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,47 +1,39 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
+  
   def create
-      @user = User.find_by(username: session_params[:username])
-    
-      if @user && @user.authenticate(session_params[:password])
-        login!
-        render json: {
-          logged_in: true,
-          user: @user
-        }
-      else
-        render json: { 
-          status: 401,
-          errors: ['no such user']
-        }
-      end
-    end
+    user = User.find_by(username: params["username"]).try(:authenticate, params["user"]["password"])
 
-  def is_logged_in?
-      if logged_in? && current_user
-        render json: {
-          logged_in: true,
-          user: current_user
-        }
-      else
-        render json: {
-          logged_in: false,
-          message: 'no such user'
-        }
-      end
-    end
-
-  def destroy
-      logout!
+    if user
+      session[:user_id] = user.id 
       render json: {
-        status: 200,
-        logged_out: true
+        status: :created,
+        logged_in: true,
+        user: user
       }
+    else
+      render json: { status: 401 }
+    end
   end
 
-  private
-
-  def session_params
-    params.require(:user).permit(:username, :password)
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
   end
 
+  def logout
+    reset_session
+    render json: { 
+      status: 200, 
+      logged_out: true 
+    }
   end
+end
