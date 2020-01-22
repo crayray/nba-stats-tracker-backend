3c915ec685a1dfb36f38203656092de675a13d16
rachfuerst <rach.fuerst@gmail.com>
2020-01-22 15:28:55 -0600

    Updating the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,39 +1,39 @@
 class SessionsController < ApplicationController
-    include CurrentUserConcern
-    
-    def create
-      user = User.find_by(username: params["usernam"]).try(:authenticate, params["user"]["password"])
+  include CurrentUserConcern
   
-      if user
-        session[:user_id] = user.id 
-        render json: {
-          status: :created,
-          logged_in: true,
-          user: user
-        }
-      else
-        render json: { status: 401 }
-      end
+  def create
+    user = User.find_by(email: params["username"]).try(:authenticate, params["user"]["password"])
+
+    if user
+      session[:user_id] = user.id 
+      render json: {
+        status: :created,
+        logged_in: true,
+        user: user
+      }
+    else
+      render json: { status: 401 }
     end
-  
-    def logged_in
-      if @current_user
-        render json: {
-          logged_in: true,
-          user: @current_user
-        }
-      else
-        render json: {
-          logged_in: false
-        }
-      end
-    end
-  
-    def logout
-      reset_session
-      render json: { 
-        status: 200, 
-        logged_out: true 
+  end
+
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
       }
     end
   end
+
+  def logout
+    reset_session
+    render json: { 
+      status: 200, 
+      logged_out: true 
+    }
+  end
+end
